a01c7a4f73c0b23fa8e6fc4c3022a32f91280d27
akrm al-hakimi <alhakimiakrmj@gmail.com>
2025-08-03 14:48:48 -0400

    feat: command line argument parsing

--- src/server/runtime.rs
+++ src/server/runtime.rs
@@ -1,7 +1,42 @@
 use crate::server::serve::Server;
 use tokio::net::TcpListener;
+use std::env;
+use std::error::Error;
+use std::path::Path;
 
-pub async fn run(addr: String) -> std::io::Result<()> {
+const USAGE: &str = r#"
+USAGE: cargo run <command> [args]
+
+Options: 
+        directory <DIR>    File serving directory (default: .)
+        -h, help           Show This help message
+"#;
+
+pub async fn run(addr: String) -> Result<(), Box<dyn Error>> {
+    
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("{}", USAGE);
+        std::process::exit(1);
+    }
+
+    match args[1].as_str() {
+        "directory" => {
+            if args.len() >= 3 {
+                env::set_current_dir(Path::new(&args[2]))?;
+                println!("Serving from directory: {}", &args[2]);
+            } else {
+                env::set_current_dir(Path::new("."))?;
+                eprintln!("Missing directory path after 'directory', defaulting to current directory.")
+            } 
+        }
+        "-h" | "help" => {
+            println!("{USAGE}");
+            return Ok(())
+        }
+        _ => eprintln!("Unknown option: {}\n{USAGE}", args[1])
+    }
+
     let addr = addr.to_string();
     let listener = TcpListener::bind(&addr).await?;
     let server = Server::new();
