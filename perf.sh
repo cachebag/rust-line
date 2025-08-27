5333e543fbc8e90058fa1c34d306352d2c2dfd27
akrm al-hakimi <alhakimiakrmj@gmail.com>
2025-08-27 09:31:08 -0400

    refactor: perf test serves index.html now

--- perf.sh
+++ perf.sh
@@ -7,19 +7,20 @@ ADDR="127.0.0.1:$PORT"
 DURATION=15s
 CONNECTIONS=250
 THREADS=10
-URL="http://$ADDR/ping"
+URL="http://$ADDR/files/index.html"
 BINARY=target/release/rustline
+SERVE_DIR=~/Desktop
 
 echo "----Building server...----"
 cargo build --release
 
 echo "----Starting server in background...----"
-$BINARY > /dev/null 2>&1 & 
+$BINARY directory "$SERVE_DIR" > /dev/null 2>&1 &
 SERVER_PID=$!
 
 sleep 1 
 
-echo "----Running wrk load test...----"
+echo "----Running wrk load test on $URL ----"
 wrk -c "$CONNECTIONS" -t "$THREADS" -d "$DURATION" "$URL"
 
 echo "----Stopping server...----"
